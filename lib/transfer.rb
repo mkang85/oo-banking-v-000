e018e4296e72da5fa23e8831659422851ed9cfdb
Michael Kang <michaelkang85@gmail.com>
2018-02-19 18:17:33 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end
 
   def execute_transaction
-    if @valid = false
+    if @valid? = false
       @status = 'rejected'
       "Transaction rejected. Please check your account balance."
     else
