18581d493f90df7a38b0d93a503883d7ead8c514
Michael Kang <michaelkang85@gmail.com>
2018-02-19 17:46:35 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,9 +1,18 @@
 class BankAccount
   attr_accessor :status, :balance
   attr_reader :name
+
   def initialize(name)
     @name = name
     @status = 'open'
     @balance = 1000
+  end
+
+  def deposit
+
+  end
+
+  def display_balance
+    @balance
   end
 end
